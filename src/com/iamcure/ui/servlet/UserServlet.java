ec678b13c7c4f8aeb456d78354ae91bbd63f9972
knclanil <knclanil@gmail.com>
2017-09-02 01:07:40 +0530

    fixes.

--- src/com/iamcure/ui/servlet/UserServlet.java
+++ src/com/iamcure/ui/servlet/UserServlet.java
@@ -35,6 +35,13 @@ public class UserServlet extends HttpServlet {
 	}
 
 	/**
+	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doGet1(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+	}
+
+	/**
 	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
 	 */
 	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
